e7280236b418192a23f4df1edd4b16f1e1cfa124
Thomas_Ju <yuanping@umich.edu>
2021-09-02 10:02:06 -0400

    add backtrack to equation prover

--- EquationProver.py
+++ EquationProver.py
@@ -54,13 +54,11 @@ class Prover():
 
     def prove(self, statement):
         left, right = tuple(statement[:statement.find('=')].split()), tuple(statement[statement.find('=')+1:].split())
-        queue = [left]
+        queue = [(left, [left])]
         checked = set()
 
-        while len(queue)!=0 and queue[0] != right:
-            current, queue = queue[0], queue[1:]
-            print(queue)
-            print('')
+        while len(queue)!=0 and queue[0][0] != right:
+            current, trace, queue = queue[0][0], queue[0][1], queue[1:]
             
             indices = [i for i, e in enumerate(current) if e == ')'][::-1]
             for i, piece in enumerate(current):
@@ -71,7 +69,7 @@ class Prover():
                     for possible in possibles:
                         possible = tuple(list(current[:i]) + list(possible) + list(current[indices[0]+1:]))
                         if possible not in checked and len(possible) < 30:
-                            queue.append(possible)
+                            queue.append((possible, trace+[possible]))
                     indices = indices[1:]
                     
                 elif piece != ',' and piece !=')':
@@ -81,18 +79,27 @@ class Prover():
                     for possible in possibles:
                         possible = tuple(list(current[:i]) + list(possible) + list(current[i+1:]))
                         if possible not in checked and len(possible) < 30:
-                            queue.append(possible)
+                            queue.append((possible, trace+[possible]))
         
                 checked.add(current)
-        return not len(queue) == 0
+                
+        if len(queue) != 0:
+            return True, queue[0][1]
+        else:
+            return False, None
+    
+    def format_proof(trace):
+        return '  ' + '\n'.join(['= '+' '.join(t) for t in trace])[2:]
+        
 
 def main():
     p1 = Prover(['Next( "1" ) = "2"', 'Next( "0" ) = "1"', 'Add( _x_ , "0" ) = _x_'\
                 , 'Add( _x_ , Next( _y_ ) ) = Next( Add( _x_ , _y_ ) )'])
-    print(p1.prove('Add( "1" , "1" ) = "2"'))
-    
-    # p2 = Prover(['"a" = "1"'])
-    # print(p2.prove('Add( "a" , "a" ) = Add( "1" , "1" )'))
+    solution, trace = p1.prove('Add( "1" , "1" ) = "2"')
+    if solution:
+        print(Prover.format_proof(trace))
+    else:
+        print("This equation can't be prove")
 
 if __name__ == "__main__":
     main()
